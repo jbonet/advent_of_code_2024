48d2916590ffda9f4f457037e431b1fef1dbbf49
Javier Bonet <javier@jbonet.xyz>
2024-12-01 11:15:35 +0100

    Improvements

--- lib/advent_of_code_2024/days/one.ex
+++ lib/advent_of_code_2024/days/one.ex
@@ -11,7 +11,7 @@ defmodule AdventOfCode2024.Days.One do
 
   def part1({input_A, input_B}) do
     Enum.zip(Enum.sort(input_A), Enum.sort(input_B))
-    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Stream.map(fn {a, b} -> abs(a - b) end)
     |> Enum.sum()
   end
 
@@ -19,17 +19,14 @@ defmodule AdventOfCode2024.Days.One do
     frequencies = Enum.frequencies(input_B)
 
     input_A
-    |> Enum.map(fn x -> x * Map.get(frequencies, x, 0) end)
+    |> Stream.map(fn x -> x * Map.get(frequencies, x, 0) end)
     |> Enum.sum()
   end
 
   def parse_input(input) do
     input
-    |> Stream.map(fn line ->
-      line |> String.split() |> Enum.map(&String.to_integer/1) |> List.to_tuple()
-    end)
-    |> Enum.reduce({[], []}, fn {item_A, item_B}, {list_A, list_B} ->
-      {[item_A | list_A], [item_B | list_B]}
-    end)
+    |> Stream.map(fn line -> line |> String.split() |> Enum.map(&String.to_integer/1) end)
+    |> Stream.map(&List.to_tuple/1)
+    |> Enum.unzip()
   end
 end
